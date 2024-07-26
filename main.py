41ac8ec54f065c10ff5b4505a959a49ed86fdbd9
Aayush Ujjwal <99407550+aayushujjwal@users.noreply.github.com>
2024-07-26 14:33:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ import streamlit as st
 # from PIL import Image
 
 # Create the caption pipeline
-caption = pipeline("image-to-text", model="ydshieh/vit-gpt2-coco-en")
+caption = transformers.pipeline("image-to-text", model="ydshieh/vit-gpt2-coco-en")
 
 # Display the image using Streamlit
 uploaded_image = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 if uploaded_image is not None:
-    image = Image.open(uploaded_image)
+    image = PIL.Image.open(uploaded_image)
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
     # Generate the caption
